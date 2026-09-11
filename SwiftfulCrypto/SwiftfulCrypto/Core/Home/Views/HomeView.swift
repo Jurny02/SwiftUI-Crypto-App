287fd03aaa35c6247f3f4d43ce221b2395a8263a
Bartosz <bartosz.jurczyk154@gmail.com>
2026-09-11 23:59:47 +0200

    feat: CoinModelView list

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct HomeView: View {
 
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
 
     var body: some View {
@@ -21,7 +22,20 @@ struct HomeView: View {
 
                 homeHeader
 
-                Spacer(minLength: 0)
+                columnTitles
+
+                if !showPortfolio {
+                    allCoinsList
+                        .transition(.move(edge: .leading))
+                }
+
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
+
+
+                Spacer()
             }
         }
     }
@@ -53,11 +67,67 @@ extension HomeView {
         }
         .padding(.horizontal)
     }
+
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(
+                        .init(
+                            top: 10,
+                            leading: 0,
+                            bottom: 10,
+                            trailing: 10
+                        )
+                    )
+            }
+        }
+        .listStyle(.plain)
+    }
+
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(
+                        .init(
+                            top: 10,
+                            leading: 0,
+                            bottom: 10,
+                            trailing: 10
+                        )
+                    )
+            }
+        }
+        .listStyle(.plain)
+    }
+
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .containerRelativeFrame(.horizontal) { length, axis in
+                    length / 3.5
+                }
+        }
+        .font(.caption)
+        .foregroundStyle(Color.theme.secondaryText)
+        .padding(.horizontal)
+    }
 }
 
-#Preview {
-    NavigationView {
-        HomeView()
-            .toolbar(.hidden)
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .toolbar(.hidden)
+        }
+        .environmentObject(dev.homeVM)
     }
 }
